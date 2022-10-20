17b5e1068301f29ec62ce83d5d47d56d112b53f9
Tristan Findley <tfindley@ukcloud.com>
2022-10-20 15:09:59 +0100

    fix: revert repo for Demo

--- core/views.py
+++ core/views.py
@@ -2,13 +2,5 @@ from django.http import HttpResponse
 
 
 def home(request):
-    text = """<h1>Welcome to our mind blowing home page</h1>
-        <br>
-        <iframe src="https://giphy.com/embed/QMHoU66sBXqqLqYvGO"
-        width="480" height="270" frameBorder="0"
-        class="giphy-embed" allowFullScreen></iframe>
-        <p>
-        <a href="https://giphy.com/gifs/this-is-fine-QMHoU66sBXqqLqYvGO">
-        via GIPHY</a>
-        </p>"""
+    text = """<h1>Welcome to our mind blowing home page</h1>"""
     return HttpResponse(text)
